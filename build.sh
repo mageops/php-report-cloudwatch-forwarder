8ef0ec1dd821bae295a2fd3c16d979dc77a55ecc
Monika Sadlok <monika.sadlok@creativestyle.pl>
2025-07-16 09:45:39 +0200

    global phar-builder installation

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 cd "$DIR"
 
 docker build -t aws-execfwd-build -f build.dockerfile .
-docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build install
-docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build build
+docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build composer update
+docker run --rm -it -e COMPOSER_HOME=/tmp/composer_home -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build /bin/sh -c "composer global require macfja/phar-builder && php -d phar.readonly=0 /tmp/composer_home/vendor/bin/phar-builder package"
