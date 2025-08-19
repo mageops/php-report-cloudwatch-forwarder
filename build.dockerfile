b6a0529d61c43f8a2c34fbbb1c401db798073e92
Piotr Rogowski <piotr.rogowski@creativestyle.pl>
2025-08-19 11:17:16 +0200

    Update dependencies, replace phat builder
    macfja/phar-builder is no longer maintained, it was replaced.
    symfony/console updated to version supporting php 8.4 and compatable with phar-builder

--- build.dockerfile
+++ build.dockerfile
@@ -4,5 +4,8 @@ FROM composer:2
 # Install the BCMath extension
 RUN docker-php-ext-install bcmath
 
+# Configure git to trust any directory
+RUN git config --global --add safe.directory '*'
+
 # build with: docker build -t aws-execfwd-build -f build.dockerfile .
 # Execute with: docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build build
--- build.sh
+++ build.sh
@@ -3,6 +3,21 @@ set -euo pipefail
 DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 cd "$DIR"
 
+# Build the Docker image
 docker build -t aws-execfwd-build -f build.dockerfile .
-docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build install
-docker run --rm -it -e COMPOSER_PROCESS_TIMEOUT=0 -w $PWD -v $PWD:$PWD aws-execfwd-build build
+
+# Create a container with long sleep to keep it alive
+CONTAINER=$(docker create aws-execfwd-build sleep 7200)
+docker cp . $CONTAINER:/app/
+
+# Start container and run commands
+docker start $CONTAINER
+docker exec $CONTAINER composer install --working-dir=/app
+docker exec $CONTAINER composer build --working-dir=/app
+
+# Copy the build artifact back
+mkdir -p build
+docker cp $CONTAINER:/app/build/aws-excfwd ./build/
+
+# Clean up
+docker rm -f $CONTAINER
